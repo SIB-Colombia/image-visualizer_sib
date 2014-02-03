8bd94d3f3c136ae659787b25843fcbfa3e4c7c81
Laura Valentina Grajales Olarte <valegrajales@gmail.com>
2014-02-03 11:26:13 -0500

    Adding new config folder

---
+++ config/environments/all.js
@@ -0,0 +1,32 @@
+var express = require('express')
+  , path = require('path');
+
+module.exports = function(parent) {
+	parent.set('port', process.env.PORT || appConfigVars.port);
+	parent.set('view engine', 'jade');
+	parent.set('jsonp callback', true );
+	parent.use(express.compress());
+	parent.use(express.favicon());
+	parent.use(express.logger('dev'));
+	parent.use(express.bodyParser());
+	parent.use(express.methodOverride());
+	parent.use(require('stylus').middleware(__dirname + '/../../public'));
+
+	var env = process.env.NODE_ENV || 'development';
+
+	// Load configuration according to environment
+	console.log("Current node environment:");
+	console.log(process.env.NODE_ENV);
+	if(process.env.NODE_ENV == 'development') {
+		require('./development')(parent);
+	} else if(process.env.NODE_ENV == 'production') {
+		require('./production')(parent);
+	} else {
+		require('./development')(parent);
+	}
+
+	// load controllers
+	require('./../routers')(parent, { verbose: true });
+
+	logger.info("Image blog sib initial configuration loaded.");
+};
